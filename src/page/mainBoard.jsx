76523dd3ac69ab388d4770df3ecf04c4e06e9c38
liaotingwei <ryougishiki23333@gmail.com>
2024-02-02 18:34:48 +0800

    feat: 切换位置

---
+++ src/page/mainBoard.jsx
@@ -0,0 +1,11 @@
+const MainBoard = () => {
+  return (
+    <>
+      <div className="text-red-500">asfds</div>
+      <img src="/mainBoard/mainEdge.png" className="w-[1080px] h-[634.5px]" />
+    </>
+  );
+};
+
+export default MainBoard;
+// 720*1.5 = 1080
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ],
+      },
+    },
+  },
 })

37fd5b8605fcbbea45a695a0391bdc6022cda9a6
liaotingwei <ryougishiki23333@gmail.com>
2024-05-08 18:25:12 +0800

    feat: 写readme

--- src/page/mainBoard.jsx
+++ src/page/mainBoard.jsx
@@ -1,9 +1,30 @@
-const MainBoard = () => {
+import React, { useState, useEffect } from 'react';
+import { Stage, Layer, Rect, Circle } from 'react-konva';
+
+function MainBoard() {
+  const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty dependency array means this effect runs once on mount and clean up on unmount
+
   return (
-    <>
-      <div className="w-[200px] bg-black">123123123</div>
-    </>
+    <Stage width={size[0]} height={size[1]}>
+      <Layer>
+        <Rect width={50} height={50} fill="red" />
+        <Circle x={200} y={200} stroke="black" radius={50} />
+      </Layer>
+    </Stage>
   );
-};
+}
 
 export default MainBoard;
